14cc593eaa520d401dfc5a0306e045b5c1c95154
Tori Cox <vgcox@umich.edu>
2018-11-15 12:44:19 -0500

    populated Bars table

--- proj3_choc.py
+++ proj3_choc.py
@@ -58,16 +58,16 @@ def populate_choc_db():
         Area REAL)'''
     cur.execute(statement)
     conn.commit()
-create_choc_db()
-populate_choc_db()
-    # with open("teams.csv") as t:
-    #     csvReader = csv.reader(t)
-    #     conn = sqlite.connect('coxtori_big10.sqlite')
-    #     cur = conn.cursor()
-    #     for row in csvReader:
-    #         statement = '''INSERT INTO Teams(Seed, Name, ConfRecord) VALUES (?,?,?) '''
-    #         cur.execute(statement, (row[0], row[1], row[2]))
-    #         conn.commit()
+    with open("flavors_of_cacao_cleaned.csv") as f:
+        conn = sqlite.connect(DBNAME)
+        cur = conn.cursor()
+        csvReader = csv.reader(f)
+        next(csvReader)
+        for row in csvReader:
+            statement = '''INSERT INTO Bars(Company,SpecificBeanBarName,REF,ReviewDate,CocoaPercent,CompanyLocationId,Rating,BeanType,BroadBeanOriginId)
+            VALUES (?,?,?,?,?,?,?,?,?) '''
+            cur.execute(statement, (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]))
+            conn.commit()
     # with open("games.csv") as g:
     #     csvReader = csv.reader(g)
     #     conn = sqlite.connect('coxtori_big10.sqlite')
@@ -76,9 +76,10 @@ populate_choc_db()
     #         statement = '''INSERT INTO Games(Winner, Loser, WinnerScore, LoserScore, Round,Time) VALUES(?,?,?,?,?,?)'''
     #         cur.execute(statement, (row[0], row[1], row[2], row[3], row[4], row[5]))
     #         conn.commit()
-    # conn.commit()
-    # conn.close()
-
+    conn.commit()
+    conn.close()
+create_choc_db()
+populate_choc_db()
 # Part 2: Implement logic to process user commands
 def process_command(command):
     return []
