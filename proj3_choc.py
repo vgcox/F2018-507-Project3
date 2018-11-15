1ba56b7032e07c497f07ca6f4c8a046db724b738
Tori Cox <vgcox@umich.edu>
2018-11-15 10:50:42 -0500

    added Bars Table

--- proj3_choc.py
+++ proj3_choc.py
@@ -29,7 +29,53 @@ def create_choc_db():
         print("Database initiated")
     except:
         print("Error, database not created correctly")
-create_choc_db()
+def populate_choc_db():
+    conn = sqlite.connect(DBNAME)
+    cur = conn.cursor()
+    statement = '''
+    CREATE TABLE Bars (
+        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+        Company TEXT,
+        SpecificBeanBarName TEXT,
+        REF TEXT,
+        ReviewDate TEXT,
+        CocoaPercent REAL,
+        CompanyLocationId INTEGER,
+        Rating REAL,
+        BeanType TEXT,
+        BroadBeanOriginId INTEGER);'''
+    cur.execute(statement)
+    conn.commit()
+populate_choc_db()
+    # statement = '''
+    # CREATE TABLE Games (
+    #     Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    #     Winner INTEGER NOT NULL,
+    #     Loser INTEGER NOT NULL,
+    #     WinnerScore INTEGER NOT NULL,
+    #     LoserScore INTEGER NOT NULL,
+    #     Round INTEGER NOT NULL,
+    #     Time TEXT NOT NULL)'''
+    # cur.execute(statement)
+    # conn.commit()
+    # with open("teams.csv") as t:
+    #     csvReader = csv.reader(t)
+    #     conn = sqlite.connect('coxtori_big10.sqlite')
+    #     cur = conn.cursor()
+    #     for row in csvReader:
+    #         statement = '''INSERT INTO Teams(Seed, Name, ConfRecord) VALUES (?,?,?) '''
+    #         cur.execute(statement, (row[0], row[1], row[2]))
+    #         conn.commit()
+    # with open("games.csv") as g:
+    #     csvReader = csv.reader(g)
+    #     conn = sqlite.connect('coxtori_big10.sqlite')
+    #     cur = conn.cursor()
+    #     for row in csvReader:
+    #         statement = '''INSERT INTO Games(Winner, Loser, WinnerScore, LoserScore, Round,Time) VALUES(?,?,?,?,?,?)'''
+    #         cur.execute(statement, (row[0], row[1], row[2], row[3], row[4], row[5]))
+    #         conn.commit()
+    # conn.commit()
+    # conn.close()
 
 # Part 2: Implement logic to process user commands
 def process_command(command):
